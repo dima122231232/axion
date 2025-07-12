805ffe4a5d86a9d7e16a02c07036f85ed65c7769
dima122231232 <115538623+dima122231232@users.noreply.github.com>
2025-07-12 21:01:45 +0300

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,14 @@
 if (!window.matchMedia('(max-aspect-ratio: 1/1), (max-width: 780px)').matches){
     document.querySelector('.content').style.height = `${document.querySelector('.content').offsetHeight + (window.outerHeight - window.innerHeight)}px`;}
-    ScrollSmoother.create({
-    wrapper: '.wrapper',
-    content: '.content',
-    smooth: 1.5,
-    effects: true, 
-    smoothTouch: 0.1
-    });
     
+    if(ScrollTrigger.isTouch !==1){
+        ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.5,
+        effects: true
+    });
+    }
 // }
 // else{
 //     const smoother = ScrollSmoother.create({
