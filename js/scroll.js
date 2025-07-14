92ee7d29b88043c46e8e4f8ca45294a1f446da65
dima122231232 <115538623+dima122231232@users.noreply.github.com>
2025-07-14 13:34:29 +0300

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,16 @@
 // if (!window.matchMedia('(max-aspect-ratio: 1/1), (max-width: 780px)').matches){
 //     document.querySelector('.content').style.height = `${document.querySelector('.content').offsetHeight + (window.outerHeight - window.innerHeight)}px`;}
     
-    if(ScrollTrigger.isTouch !==1){
-        ScrollSmoother.create({
-        wrapper: '.wrapper',
-        content: '.content',
-        smooth: 1.5,
-        effects: true
-    });
-    }
-// }
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+if(ScrollTrigger.isTouch !==1){
+    ScrollSmoother.create({
+        wrapper: '#wrapper',
+        content: '#content',
+        smooth:1.5,
+        effects:true,
+    })
+}
+
 // else{
 //     const smoother = ScrollSmoother.create({
 //     wrapper: '.wrapper',
