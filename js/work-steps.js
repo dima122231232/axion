21e945f6d254e3d64a008d7b261d4a49e3b1414a
dima122231232 <115538623+dima122231232@users.noreply.github.com>
2025-07-14 12:45:26 +0300

    Add files via upload

---
+++ js/work-steps.js
@@ -0,0 +1,62 @@
+gsap.set('.work-steps__column--left .img-wrapper', {rotateY: 180,transformOrigin: 'center center'});
+
+if(!isMobile){gsap.set('.work-steps__item', {y:.3*vh});}else{gsap.set('.work-steps__item', {y:.6*vh})}
+if(!isMobile){
+    document.querySelectorAll('.work-steps__column--left .img-wrapper, .work-steps__column--right .img-wrapper').forEach(wrapper => {
+    const img = wrapper.querySelector('img');
+    let enterTween, leaveTween;
+
+    wrapper.addEventListener('mouseenter', () => {
+        leaveTween?.kill();
+        enterTween = gsap.to(img, {
+        duration: 0.3,
+        scale: 1,
+        rotateY: -50,
+        transformOrigin: 'left center',
+        ease: "power2.inOut",
+        overwrite: true
+        });
+    });
+
+    wrapper.addEventListener('mouseleave', () => {
+        const playLeave = () => {
+        leaveTween = gsap.to(img, {
+            duration: 0.5,
+            scale: 1,
+            rotateY: 0,
+            ease: "power2.inOut",
+            overwrite: true
+        });
+        };
+        enterTween?.isActive() ? enterTween.eventCallback("onComplete", playLeave) : playLeave();
+    });
+    });
+    gsap.to(".work-steps", {scrollTrigger: {trigger: ".work-steps",start: "top top",end: () => `${document.querySelector(".work-steps").offsetHeight}px top`,pin: true,scrub: false,}});
+
+    const tl = gsap.timeline({scrollTrigger: {trigger: ".work-steps",start: "top top",end: `bottom+=${0.15 * vh} top`,scrub: true,}});
+    gsap.utils.toArray(".work-steps__item").forEach((item, i) => {tl.to(item, {y: 0 * vh,ease: "power2.out"}, i * .2);
+    });
+    gsap.from(".work-steps__column--right .img-wrapper img, .work-steps__column--left .img-wrapper img", {opacity: 0,x:-110,duration: 1,ease: "power2.out",scrollTrigger: {trigger: ".work-steps",start: "top 30%",  once: true}});
+}
+else{ 
+    gsap.fromTo(".work-steps",{}, {
+    y: "100vh",
+    ease:"none",
+    scrollTrigger: {
+    trigger: ".work-steps",
+    start: "top top",     // когда элемент входит снизу
+    end: () => `+=${document.querySelector(".work-steps").offsetHeight}`,
+    scrub: true,             // плавная анимация при скролле
+    markers: true
+  }
+});
+const tl = gsap.timeline({scrollTrigger: {trigger: ".work-steps",start: "top top",end: `bottom+=${0.15 * vh} top`,scrub: true,}});
+    gsap.utils.toArray(".work-steps__item").forEach((item, i) => {tl.to(item, {y: 0 * vh,ease: "power2.out"}, i * .2);});
+    gsap.from(".work-steps__column--right .img-wrapper img, .work-steps__column--left .img-wrapper img", {opacity: 0,x:-110,duration: 1,ease: "power2.out",scrollTrigger: {trigger: ".work-steps",start: "top 60%",  once: true}});
+}
+
+
+
+
+animateWords(".work-steps__title", ".work-steps", "top 50%");
+animateWords(".work-steps__text", ".work-steps", "top 30%");
